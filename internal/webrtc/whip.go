18b6ed9807ad828be1abb699dc61a8115df6ca74
Sean DuBois <sean@siobud.com>
2024-04-11 13:43:04 -0400

    Use inbound JitterBuffer

--- internal/webrtc/whip.go
+++ internal/webrtc/whip.go
@@ -2,12 +2,13 @@ package webrtc
 
 import (
 	"errors"
+	"fmt"
 	"io"
 	"log"
 	"strings"
 
+	"github.com/pion/interceptor/pkg/jitterbuffer"
 	"github.com/pion/rtcp"
-	"github.com/pion/rtp"
 	"github.com/pion/webrtc/v4"
 )
 
@@ -60,13 +61,14 @@ func videoWriter(remoteTrack *webrtc.TrackRemote, stream *stream, peerConnection
 		}
 	}()
 
-	rtpBuf := make([]byte, 1500)
-	rtpPkt := &rtp.Packet{}
 	lastTimestamp := uint32(0)
 	codec := getVideoTrackCodec(remoteTrack.Codec().RTPCodecCapability.MimeType)
 
+	jitterBuffer := jitterbuffer.New()
+	lastSequenceNumber := uint16(0)
+
 	for {
-		rtpRead, _, err := remoteTrack.Read(rtpBuf)
+		recvPkt, _, err := remoteTrack.ReadRTP()
 		switch {
 		case errors.Is(err, io.EOF):
 			return
@@ -75,27 +77,46 @@ func videoWriter(remoteTrack *webrtc.TrackRemote, stream *stream, peerConnection
 			return
 		}
 
-		if err = rtpPkt.Unmarshal(rtpBuf[:rtpRead]); err != nil {
-			log.Println(err)
-			return
+		jitterBuffer.Push(recvPkt)
+
+		for {
+			popPkt, err := jitterBuffer.Pop()
+			if err != nil || popPkt == nil {
+				if !errors.Is(err, jitterbuffer.ErrPopWhileBuffering) {
+					timestampDifference := int64(recvPkt.Timestamp) - int64(lastTimestamp)
+					if float64(timestampDifference)/float64(remoteTrack.Codec().ClockRate) >= .7 {
+						//fmt.Println(timestampDifference, recvPkt.Timestamp, lastTimestamp)
+						jitterBuffer.SetPlayoutHead(jitterBuffer.PlayoutHead() + 1)
+					}
+				}
+				break
+			}
+
+			if lastSequenceNumber+1 != popPkt.SequenceNumber && false {
+				fmt.Println(lastSequenceNumber, popPkt.SequenceNumber)
+			}
+
+			videoTrack.packetsReceived.Add(1)
+
+			popPkt.Extension = false
+			popPkt.Extensions = nil
+
+			timeDiff := popPkt.Timestamp - lastTimestamp
+			if lastTimestamp == 0 {
+				timeDiff = 0
+			}
+			if timeDiff != 0 {
+				fmt.Println(timeDiff)
+			}
+			lastTimestamp = popPkt.Timestamp
+			lastSequenceNumber = popPkt.SequenceNumber
+
+			s.whepSessionsLock.RLock()
+			for i := range s.whepSessions {
+				s.whepSessions[i].sendVideoPacket(popPkt, id, timeDiff, codec)
+			}
+			s.whepSessionsLock.RUnlock()
 		}
-
-		videoTrack.packetsReceived.Add(1)
-
-		rtpPkt.Extension = false
-		rtpPkt.Extensions = nil
-
-		timeDiff := rtpPkt.Timestamp - lastTimestamp
-		if lastTimestamp == 0 {
-			timeDiff = 0
-		}
-		lastTimestamp = rtpPkt.Timestamp
-
-		s.whepSessionsLock.RLock()
-		for i := range s.whepSessions {
-			s.whepSessions[i].sendVideoPacket(rtpPkt, id, timeDiff, codec)
-		}
-		s.whepSessionsLock.RUnlock()
 	}
 }
 
